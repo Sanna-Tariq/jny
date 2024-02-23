59183c21776b768b17b8532f4c9f4ce323932c76
Sanna-Tariq <sannatariq649@gmail.com>
2024-02-23 17:22:12 +0530

    removed other extra favicon images

--- composables/dark-color-scheme-check.ts
+++ composables/dark-color-scheme-check.ts
@@ -6,5 +6,5 @@
   const prefersDark = matchMedia('(prefers-color-scheme: dark)').matches
   const setting = localStorage.getItem('vueuse-color-scheme') || 'auto'
   if (setting === 'dark' || (prefersDark && setting !== 'light'))
-    document.documentElement.classList.toggle('dark', false)
+    document.documentElement.classList.toggle('dark', true)
 })()
