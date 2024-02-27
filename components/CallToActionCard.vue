e01e8b965c91c1c4727517f720eb359987a832ce
Sanna-Tariq <sannatariq649@gmail.com>
2024-02-27 10:18:47 +0530

    removed link for featured products

--- components/CallToActionCard.vue
+++ components/CallToActionCard.vue
@@ -41,13 +41,13 @@ const bgStyles = imageOptimized.value.bgStyles
   <div class="pb-4 px-4 rounded-3xl w-full sm:w-8/12 md:w-6/12 lg:w-4/12">
     <div class="bg-cover bg-no-repeat flex pb-4 pt-96 px-4 rounded-3xl shadow-md"
       :style="{ backgroundImage: `url(${img})` }">
-      <NuxtLink :to="to" class="group mx-auto w-5/6">
-        <BaseButton size="xl" class="font-semibold rounded-lg w-full" block>
-          <div class="font-semibold my-2 text-center">
-            {{ action }}
-          </div>
-        </BaseButton>
-      </NuxtLink>
+      <!-- <NuxtLink :to="#" class="group mx-auto w-5/6"> -->
+      <BaseButton size="xl" class="font-semibold rounded-lg w-full" block>
+        <div class="font-semibold my-2 text-center">
+          {{ action }}
+        </div>
+      </BaseButton>
+      <!-- </NuxtLink> -->
     </div>
   </div>
 </template>
